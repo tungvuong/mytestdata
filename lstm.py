a34e93563be928cc5b5177c7816bb2260b15761a
root <root@heel.hiit.fi>
2023-08-01 11:22:39 +0300

    lstm test 2

--- lstm.py
+++ lstm.py
@@ -22,6 +22,8 @@ import numpy as np
 import tensorflow as tf
 
 
+tf.compat.v1.disable_eager_execution()
+
 class ModelNetwork:
     """
     RNN with num_layers LSTM layers and a fully-connected output layer
@@ -49,32 +51,32 @@ class ModelNetwork:
         self.lstm_last_state = np.zeros(
             (self.num_layers * 2 * self.lstm_size,)
         )
-        with tf.variable_scope(self.scope):
+        with tf.compat.v1.variable_scope(self.scope):
             # (batch_size, timesteps, in_size)
-            self.xinput = tf.placeholder(
+            self.xinput = tf.compat.v1.placeholder(
                 tf.float32,
                 shape=(None, None, self.in_size),
                 name="xinput"
             )
-            self.lstm_init_value = tf.placeholder(
+            self.lstm_init_value = tf.compat.v1.placeholder(
                 tf.float32,
                 shape=(None, self.num_layers * 2 * self.lstm_size),
                 name="lstm_init_value"
             )
             # LSTM
             self.lstm_cells = [
-                tf.contrib.rnn.BasicLSTMCell(
+                tf.compat.v1.nn.rnn_cell.BasicLSTMCell(
                     self.lstm_size,
                     forget_bias=1.0,
                     state_is_tuple=False
                 ) for i in range(self.num_layers)
             ]
-            self.lstm = tf.contrib.rnn.MultiRNNCell(
+            self.lstm = tf.compat.v1.nn.rnn_cell.MultiRNNCell(
                 self.lstm_cells,
                 state_is_tuple=False
             )
             # Iteratively compute output of recurrent network
-            outputs, self.lstm_new_state = tf.nn.dynamic_rnn(
+            outputs, self.lstm_new_state = tf.compat.v1.nn.dynamic_rnn(
                 self.lstm,
                 self.xinput,
                 initial_state=self.lstm_init_value,
@@ -82,13 +84,13 @@ class ModelNetwork:
             )
             # Linear activation (FC layer on top of the LSTM net)
             self.rnn_out_W = tf.Variable(
-                tf.random_normal(
+                tf.compat.v1.random_normal(
                     (self.lstm_size, self.out_size),
                     stddev=0.01
                 )
             )
             self.rnn_out_B = tf.Variable(
-                tf.random_normal(
+                tf.compat.v1.random_normal(
                     (self.out_size,), stddev=0.01
                 )
             )
@@ -103,7 +105,7 @@ class ModelNetwork:
                 (batch_time_shape[0], batch_time_shape[1], self.out_size)
             )
             # Training: provide target outputs for supervised training.
-            self.y_batch = tf.placeholder(
+            self.y_batch = tf.compat.v1.placeholder(
                 tf.float32,
                 (None, None, self.out_size)
             )
@@ -114,7 +116,7 @@ class ModelNetwork:
                     labels=y_batch_long
                 )
             )
-            self.train_op = tf.train.RMSPropOptimizer(
+            self.train_op = tf.compat.v1.train.RMSPropOptimizer(
                 self.learning_rate,
                 0.9
             ).minimize(self.cost)
