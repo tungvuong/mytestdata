4d2cbeb17e565783d9768bf5134f94c7f94f1482
root <root@heel.hiit.fi>
2023-08-07 12:28:20 +0300

    lstm test 2

--- lstm_predict.py
+++ lstm_predict.py
@@ -290,9 +290,9 @@ def main():
                         element = np.random.choice(range(len(vocab)), p=out)
                         pred_target += vocab[element]
                         out = net.run_step(embed_to_vocab(vocab[element], vocab), False)
+                    print(pred_target)
                     pred_targets.append(pred_target)
                 suggestions[user].append([row['title'],row['target'],pred_targets,row['source'],index+2])
-                print(pred_target)
             with open('./mytestdata/lstm3screen.json', 'w') as outfile:
                 json.dump(suggestions, outfile)
             
